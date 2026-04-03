41429f30d87fccd203513ddcc5880ab042847a50
Hangum <hangumarchanaofficial@gmail.com>
2026-04-03 18:10:19 +0530

    Update Vite configuration

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,7 +9,11 @@ export default defineConfig({
       '/api': {
         target: 'http://127.0.0.1:5000',
         changeOrigin: true,
-      }
+      },
+      '/auth': {
+        target: 'http://127.0.0.1:5001',
+        changeOrigin: true,
+      },
     }
   }
 })
